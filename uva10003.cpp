b1cfcc7625f8f48d400731f212581efc7e6ebeed
YangZhao <bit.zhaoyang512@gmail.com>
2016-05-12 18:32:38 +0300

    Update uva10003.cpp
    Both bottom-up and top-down solution were added.

--- uva10003.cpp
+++ uva10003.cpp
@@ -1,36 +1,47 @@
 #include <bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0), cout.precision(15);
-
 using namespace std;
 
-int vi[55];
 int table[55][55];
 
-int cut(int *c, int n){
-    for(int i = 0; i < n-1; ++i)    table[i][i+1] = 0;
+// recursive solution
+// int solve(int i, int j, vector<int> &v){
+//     if(i+1 == j)    return 0;
+//     if(table[i][j]) return table[i][j];
+//
+//     int ans = INT_MAX;
+//     for(int k = i+1; k < j; ++k){
+//         ans = min(ans, solve(i, k, v) + solve(k, j, v));
+//     }
+//     ans += v[j] - v[i];
+//     table[i][j] = ans;
+//     return ans;
+// }
 
-    for(int l = 2; l < n; ++l){
-        for(int i = 0; i+l < n; ++i){
-            int j = i+l;
-            table[i][j] = INT_MAX;
-            for(int k = i+1; k < j; ++k){
-                table[i][j] = min(table[i][k] + table[k][j], table[i][j]);
-            }
-            table[i][j] += c[j] - c[i];
-        }
-    }
 
-    return table[0][n-1];
-}
-
+// bottom up solution
 int main(){ _
-    int l, n;
-    while(cin >> l, l != 0){
-        cin >> n;
-        vi[0] = 0;
-        for(int i = 1; i <= n; ++i)  cin >> vi[i];
-        vi[n+1] = l;
-        cout << "The minimum cutting is " << cut(vi, n+2) << "." << endl;
+    int l;
+    while(cin >> l){
+        if(l == 0) break;
+
+        memset(table, 0, sizeof table);
+        int n;  cin >> n;
+        vector<int> v(n+2, 0);
+        for(int i = 1; i <= n; ++i)  cin >> v[i];
+        v[n+1] = l;
+
+        for(int len = 2; len < n+2; ++len){
+            for(int i = 0; i < n+2-len; ++i){
+                int j = i + len, ans = INT_MAX;
+                for(int k = i+1; k < j; ++k){
+                    ans = min(ans, table[i][k] + table[k][j]);
+                }
+                table[i][j] = ans + v[j]-v[i];
+            }
+        }
+
+        cout << "The minimum cutting is " << table[0][n+1] << "." << endl;
     }
     return 0;
 }
