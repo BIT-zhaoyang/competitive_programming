ef5655db6b36956c3d5366ebf351edc1cec9d4b3
yang.zhao <bit.zhaoyang512@gmail.com>
2017-04-28 11:11:32 +0900

    This is an interesting problem. Nice!

---
+++ uva10635.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define UNVISITED -1
+#define POS_INF 1 << 25
+#define NEG_INF -1 << 25
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0), cout.precision(15);
+using namespace std;
+
+typedef long long int64;
+typedef pair<int, int> ii;
+
+int LIS(vector<int> &vi){
+  vector<int> seq;
+  for(int i = 0; i < vi.size(); ++i){
+    int val = vi[i];
+    auto it = upper_bound(seq.begin(), seq.end(), val);
+    if(it == seq.end()) seq.push_back(val);
+    else *it = val;
+  }
+
+  // for(auto ele : seq) cout << ele << " "; cout << endl;
+
+  return seq.size();
+}
+
+int main(){ _
+  int TC; cin >> TC;
+  for(int tc = 1; tc <= TC; ++tc){
+    cout << "Case " << tc << ": ";
+    int n, p, q;  cin >> n >> p >> q;
+    vector<int> pos(n*n + 1, -1), input;
+    for(int i = 0; i <= p; ++i){
+      int val;  cin >> val;
+      pos[val] = i;
+    }
+
+    for(int i = 0; i <= q; ++i){
+      int val;  cin >> val;
+      // cout << val << "(" << pos[val] << ")" << " ";
+      if(pos[val] == -1)  continue;
+      input.push_back(pos[val]);
+    }
+
+    // cout << endl;
+    // for(int i = 0; i < pos.size(); ++i) cout << pos[i] << " ";  cout << endl;
+    // for(int i = 0; i < input.size(); ++i) cout << input[i] << " ";  cout << endl;
+
+    cout << LIS(input) << endl;
+  }
+  return 0;
+}
