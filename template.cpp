2667f6eb235d8ef55311995c0dc8455dff11f414
Yang Zhao <bit.zhaoyang512@gmail.com>
2018-09-01 20:59:27 +0900

    Changed template.cpp. Use MAX_INT, MIN_INT instead of define POS_INF, NEG_INF by myself.

--- template.cpp
+++ template.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 #define UNVISITED -1
-#define POS_INF 1 << 25
-#define NEG_INF -1 << 25
 #define _ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0), cout.precision(15);
 using namespace std;
 
